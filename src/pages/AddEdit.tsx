e1ac68c380033075b447922d73b9a66005c6c4c7
Yasu1025 <sillyjp25@gmail.com>
2022-05-26 00:49:44 +0900

    simple setting

---
+++ src/pages/AddEdit.tsx
@@ -0,0 +1,7 @@
+import React, { memo, VFC } from "react";
+
+const AddEdit: VFC = memo(() => {
+  return <div>AddEdit</div>;
+});
+
+export default AddEdit;
---
+++ src/pages/Info.tsx
@@ -0,0 +1,7 @@
+import React, { memo, VFC } from "react";
+
+const Info: VFC = memo(() => {
+  return <div>Info</div>;
+});
+
+export default Info;
